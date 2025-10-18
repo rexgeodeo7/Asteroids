826f11d2915ab73bdda03c812c0810866974a77a
rexgeodeo <177790025+rexgeodeo7@users.noreply.github.com>
2025-10-18 17:21:23 -0500

    K: added rate limiter to bullets

--- player.py
+++ player.py
@@ -7,8 +7,8 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
-
-        # in the player class
+        self.timer =  0 # rate limit
+       
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -34,8 +34,14 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
-        if keys[pygame.K_SPACE]:
-            self.shoot()
+
+        self.timer -= dt
+
+        if self.timer <= 0:
+            if keys[pygame.K_SPACE]:
+                self.shoot()
+
+
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -44,5 +50,6 @@ class Player(CircleShape):
     def shoot(self):
             velocity = (pygame.Vector2(0, 1).rotate(self.rotation)) * PLAYER_SHOOT_SPEED
             shot = Shot(self.position.x, self.position.y, velocity)
+            self.timer = PLAYER_SHOT_COOLDOWN
 
         
